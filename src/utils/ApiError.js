e419144fcf85098baa05ffcb55cfee2db49cb6ee
Mohan rana <ranamohan4341@gmail.com>
2024-08-26 08:51:29 +0545

    Api error

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,9 +3,9 @@ class ApiError extends Error {
         statusCode,
         message = "something went wrong",
         errors = [],
-        stack = ''
+        stack = ''  //A string representing the stack trace. It shows where the error occurred in the code. If not provided, it will be automatically generated.
     ){
-        super(message) // super helps to override the data
+        super(message) // l invokes the constructor of the parent Error class, passing the message to it.
         this.statusCode = statusCode
         this.data = null
         this.message = message
